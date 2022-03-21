856e9c2e8c35b44df1a892ddb8220c1f9d5063d6
jayce <jayce@wjtb.net>
2022-03-21 11:18:05 +0900

    테스트 소스 업데이트
    concentration, handlegazepoint callback 위치 수정

--- app/src/main/java/sc/artificial/lesserapitest/MainActivity.kt
+++ app/src/main/java/sc/artificial/lesserapitest/MainActivity.kt
@@ -1,14 +1,16 @@
 package sc.artificial.lesserapitest
 
+import android.graphics.PointF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import sc.artificial.lesserapi.camera.GazeAnalysisDelegate
 import sc.artificial.lesserapi.camera.GazeAnalysisManager
 import sc.artificial.lesserapi.camera.GazeAnalysisType
 import sc.artificial.lesserapi.permissions.PermissionControl
 import sc.artificial.lesserapitest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), GazeAnalysisDelegate {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +18,34 @@ class MainActivity : AppCompatActivity(), GazeAnalysisDelegate {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if (PermissionControl.checkAndGetPermissions(this)) {
-            GazeAnalysisManager.initAnalyzer(this, arrayOf(GazeAnalysisType.concentraiton, GazeAnalysisType.gaze))
+            Log.d("ksjayce_c","permission is done.")
+            GazeAnalysisManager.initAnalyzer(object : GazeAnalysisDelegate {
+                override fun handleConcentration(isConcentrating: Boolean, timestamp: Long) {
+//                    super.handleConcentration(isConcentrating, timestamp)
+                    Log.d("ksjayce_c","isconcentrating : $isConcentrating -- time : $timestamp")
+                }
+
+                override fun handleGazePoint(gazePoint: PointF, timestamp: Long) {
+//                    super.handleGazePoint(gazePoint, timestamp)
+                    Log.d("ksjayce_g","x : ${gazePoint.x}, y : ${gazePoint.y} -- time : $timestamp")
+                }
+            }, arrayOf(GazeAnalysisType.concentraiton, GazeAnalysisType.gaze))
         }
         GazeAnalysisManager.bindCamera(this)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        GazeAnalysisManager.unbindCamera()
+    }
+//
+//    override fun handleConcentration(isConcentrating: Boolean, timestamp: Long) {
+//        super.handleConcentration(isConcentrating, timestamp)
+//
+//    }
+//
+//    override fun handleGazePoint(gazePoint: PointF, timestamp: Long) {
+//        super.handleGazePoint(gazePoint, timestamp)
+//
+//    }
 }
